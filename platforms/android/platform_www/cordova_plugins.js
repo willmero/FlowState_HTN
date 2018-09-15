1ff2c63d6cb56698cef0b462ef8f9e3b2e9ddab5
Will Mero <will@roadmunk.com>
2018-09-15 16:23:54 -0400

    Initial Commit

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "institute.ambrosia.museplugin.MusePlugin",
+    "file": "plugins/institute.ambrosia.museplugin/www/MusePlugin.js",
+    "pluginId": "institute.ambrosia.museplugin",
+    "clobbers": [
+      "MusePlugin"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "institute.ambrosia.museplugin": "0.2.11"
+};
+// BOTTOM OF METADATA
+});
